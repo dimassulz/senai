c9c2a720dd319128486dcb95236b987421c3f45a
dimassulz <dimassulz@gmail.com>
2017-01-17 21:05:07 -0300

    atualizacao 17012017

--- pizzaria/js/carregarPizza.js
+++ pizzaria/js/carregarPizza.js
@@ -68,6 +68,7 @@ $(document).ready(function(){
             $('#tradicionais').html(montarHtmlPizza(tradicional));
             $('#doces').html(montarHtmlPizza(doce));
             $('#exoticas').html(montarHtmlPizza(exotica));
+            eventoClickComprar();
         },
         //função error responsável por mostrar os erros caso o AJAX não consiga trazer //as informações do arquivo pizza.json
         error: function(){
@@ -91,7 +92,7 @@ $(document).ready(function(){
                     +'<p>'+pizza.ingredientes+'</p>'
                     +'<h1 class="text-right">R$ '+pizza.preco+'</h1>'
                     +'<div class="text-right">'
-                        +'<button class="btn btn-lg btn-danger">'
+                        +'<button class="btn btn-lg btn-danger comprar">'
                             +'<span class="glyphicon glyphicon-shopping-cart"></span>'
                             +'Comprar'
                         +'</button>'
@@ -107,6 +108,40 @@ $(document).ready(function(){
         });
         //retorna o html pronto contendo todas as pizzas
         return htmlRowPizza;
+    };
+    
+    eventoClickComprar = function () {
+        bootbox.setLocale('pt'); //traduzir para portugues
+        $('.comprar').on('click', function () { //adicionando evento de click ao botao imprimir utilizando a classe cupom.
+            var nomePizza = $(this).parent().parent().find('h2').html();
+            var formBordas = '<div class="form-group"> <label class="col-md-12 control-label" for="bordas"></label> <div class="col-md-12"> <label class="radio-inline" for="bordas-0"> <input type="radio" name="bordas" id="bordas-0" value="0" checked="checked"> Sem Borda </label> <label class="radio-inline" for="bordas-1"> <input type="radio" name="bordas" id="bordas-1" value="1"> Cheddar </label> <label class="radio-inline" for="bordas-2"> <input type="radio" name="bordas" id="bordas-2" value="2"> Requeijão </label> <label class="radio-inline" for="bordas-3"> <input type="radio" name="bordas" id="bordas-3" value="3"> Chocolate </label> <label class="radio-inline" for="bordas-4"> <input type="radio" name="bordas" id="bordas-4" value="4"> Doce de Leite </label> </div></div>';
+            var formTamanho = '<div class="form-group"> <label class="col-md-12 control-label" for="tamanho"></label> <div class="col-md-12"> <label class="radio-inline" for="tamanho-0"> <input type="radio" name="tamanho" id="tamanho-0" value="0" checked="checked"> Pequena </label> <label class="radio-inline" for="tamanho-1"> <input type="radio" name="tamanho" id="tamanho-1" value="1"> Média </label> <label class="radio-inline" for="tamanho-2"> <input type="radio" name="tamanho" id="tamanho-2" value="2"> Grande </label> </div></div>';
+            var formAcomp = '<div class="form-group"> <label class="col-md-12 control-label" for="acompanhamentos"></label> <div class="col-md-12"> <label class="checkbox-inline" for="acompanhamentos-0"> <input type="checkbox" name="acompanhamentos" id="acompanhamentos-0" value="0"> Azeitona </label> <label class="checkbox-inline" for="acompanhamentos-1"> <input type="checkbox" name="acompanhamentos" id="acompanhamentos-1" value="1"> Manjericão </label> <label class="checkbox-inline" for="acompanhamentos-2"> <input type="checkbox" name="acompanhamentos" id="acompanhamentos-2" value="2"> Pepperoni </label> <label class="checkbox-inline" for="acompanhamentos-3"> <input type="checkbox" name="acompanhamentos" id="acompanhamentos-3" value="3"> Presunto </label> <label class="checkbox-inline" for="acompanhamentos-4"> <input type="checkbox" name="acompanhamentos" id="acompanhamentos-4" value="4"> Tomate Seco </label> </div></div>';
+            var formQtd = '<div class="form-group"> <label class="col-md-12 control-label" for="quantidade"></label> <div class="col-md-12"> <select id="quantidade" name="quantidade" class="form-control"> <option value="1">1</option> <option value="2">2</option> <option value="3">3</option> <option value="4">4</option> <option value="5">5</option> <option value="6">6</option> <option value="7">7</option> <option value="8">8</option> <option value="9">9</option> <option value="10">10</option> </select> </div></div>';
+            var message = '<h2>Escolha sua borda</h2>'
+                        +formBordas
+                        + '<h2>Escolha o tamanho</h2>'
+                        +formTamanho
+                        + '<h2>Adicionar acompanhamentos</h2>'
+                        +formAcomp
+                        + '<h2>Quantidade</h2>'
+                        +formQtd;
+                        
+
+            bootbox.dialog({
+                            title: "Personalize sua pizza sabor "+nomePizza, 
+                            size: "large",
+                            buttons: {
+                                "Comprar" : {
+                                    label: "Comprar",
+                                    className: "btn-success",
+                                    callback:function(){
+                                    }
+                                }
+                            },
+                            message: message
+                          });
+        });
     };
     
 });
